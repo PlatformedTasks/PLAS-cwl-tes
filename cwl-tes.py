09fa4353a17ac0ed882082c8fb3cba9e6de29468
Luca <luca13190@gmail.com>
2021-12-18 15:08:35 +0100

    Edit cwl-tes

--- cwl-tes.py
+++ cwl-tes.py
@@ -4,19 +4,6 @@ cwl-tes entrypoint script
 import sys
 import cwl_tes.main
 
-# --tes http://master-node:31567 tests/hashsplitter-workflow.cwl.yml --input tests/resources/test.txt
-sys.argv.append("--remote-storage-url")
-sys.argv.append("ftp://10.0.0.10/files/out")
-sys.argv.append("--insecure")
-sys.argv.append("--tes")
-sys.argv.append("http://10.0.0.10:31567")
-sys.argv.append("--leave-outputs")
-sys.argv.append("--debug")
-# sys.argv.append("tests/hashsplitter-workflow.cwl.yml")
-# sys.argv.append("tests/hashsplitter-sha.cwl.yml")
-sys.argv.append("tests/helm-horovod.cwl.yml")
-sys.argv.append("tests/inputs.json")
-
 
 if __name__ == "__main__":
     sys.exit(
